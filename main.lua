813aaf4b149e139d19973eae0d56d3e6cd4ef1c6
YvanDotet <40297112+YvanDotet@users.noreply.github.com>
2025-02-27 23:11:08 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,20 @@
 local List = require("List")
 
-local l=List.create()
-local l2=List({1,a="m"})
+local l1=List.create()
+local l2=List({"a","b","c"})
+
+l1:print()
+l2:print()
+
+local l3=l1+l2
+l3:append(5)
+
+local l4=List.create({"new","list"})
+l3:extend(l4)
+l3:print()
+
+local l5=l3:copy()
+print(l5:pop(5))
+print(l5:have("list"))
+l5:remove("c")
+print(l5:len())
